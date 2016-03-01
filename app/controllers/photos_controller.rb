edfeecb2f7c8ed60a5712510930869b798a460a9
Devin Mandelbaum <email": null>
2016-03-01 17:16:32 -0500

    Add display of average rating

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,6 +5,7 @@ class PhotosController < ApplicationController
 
   def show
     @photo = Photo.find(params[:id])
+    @average = @photo.avg
   end
 
   def new
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,8 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :ratings, dependent: :destroy
   validates :url, presence: true, uniqueness: true
+
+  def avg
+    self.ratings.average(:value)
+  end
 end
